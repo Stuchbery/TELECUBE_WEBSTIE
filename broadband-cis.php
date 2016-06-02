279c213bbf484b74d150c88fab4fe1068a3a2b39
Stuchbery <jamesmidship@gmail.com>
2016-06-02 15:07:56 +1000

    Add files via upload

---
+++ broadband-cis.php
@@ -0,0 +1,451 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+				<title>Telecube&#8482; - 1300 Numbers / Hosted PABX / Fax to Email</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta charset="utf-8">
+
+	    <link rel="stylesheet" href="/assets/css/bootstrap.css" media="screen">
+		<link rel="stylesheet" href="/assets/css/bootswatch.min.css">
+		<link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
+
+		<link rel="stylesheet" href="/assets/bower_components/bootstrap/less/responsive-utilities.less">
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		  <script src="/assets/bower_components/bootstrap/assets/js/html5shiv.js"></script>
+		  <script src="/assets/bower_components/bootstrap/assets/js/respond.min.js"></script>
+		<![endif]-->
+
+		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+		
+				<script> 
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ 
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); 
+			ga('create', 'UA-46530487-1', 'telecube.com.au'); 
+			ga('send', 'pageview'); 
+		</script> 
+		
+		<script type="text/javascript">
+			var __lc = {};
+			__lc.license = 1848312;
+			__lc.group = 0;
+			
+			(function() {
+			var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+			lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+			})();
+		</script>
+
+	</head>
+<body>
+
+	
+<div class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a href="/" class="navbar-brand"><span class="fa fa-home"></span> Telecube</a>
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="navbar-collapse collapse" id="navbar-main">
+      <ul class="nav navbar-nav">
+		<li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><i class="fa fa-download"></i> 1300 Numbers <span class="caret"></span></a>
+			<ul class="dropdown-menu" aria-labelledby="themes">
+				<li ><a tabindex="-1" href="/1300-numbers/"><i class="fa fa-sitemap fa-fw"></i> Overview</a></li>
+				<li ><a tabindex="-1" href="/1300-numbers/features/"><i class="fa fa-folder-open fa-fw"></i> Features</a></li>
+				<li ><a tabindex="-1" href="/1300-numbers/prices/"><i class="fa fa-dollar fa-fw"></i> Prices</a></li>
+				<li ><a tabindex="-1" href="/1300-numbers/faq/"><i class="fa fa-question-circle fa-fw"></i> Frequently Asked Questions</a></li>
+				<li ><a tabindex="-1" href="/1300-numbers/smart-numbers/"><i class="fa fa-sort-numeric-asc fa-fw"></i> Smart Numbers</a></li>
+			</ul>
+		</li>
+
+         <li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><i class="fa fa-phone"></i> VoIP <span class="caret"></span></a>
+			<ul class="dropdown-menu" aria-labelledby="themes">
+				<li ><a tabindex="-1" href="/voip/"><span class="fa fa-sitemap fa-fw"></span> Overview</a></li>
+				<li ><a tabindex="-1" href="/voip/hardware/"><i class="fa fa-hdd-o fa-fw"></i> Hardware</a></li>
+				<li ><a tabindex="-1" href="/voip/prices/"><i class="fa fa-dollar fa-fw"></i> Prices</a></li>
+				<li ><a tabindex="-1" href="/voip/international-rates/"><i class="fa fa-globe fa-fw"></i> International Rates</a></li>
+				<li ><a tabindex="-1" href="/voip/faq/"><i class="fa fa-question-circle fa-fw"></i> Frequently Asked Questions</a></li>
+			</ul>
+        </li>
+
+         <li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><i class="fa fa-cogs"></i> Other Services <span class="caret"></span></a>
+			<ul class="dropdown-menu" aria-labelledby="themes">
+				<li ><a tabindex="-1" href="/other-services/"><i class="fa fa-sitemap fa-fw"></i> Overview</a></li>
+				<li ><a tabindex="-1" href="/other-services/ivr/"><i class="fa fa-comments-o fa-fw"></i> Auto Attendant (IVR)</a></li>
+				<li ><a tabindex="-1" href="/other-services/fax/"><i class="fa fa-print fa-fw"></i> Fax Receive</a></li>
+				<li ><a tabindex="-1" href="/other-services/voicemail/"><i class="fa fa-envelope fa-fw"></i> Voicemail to Email</a></li>
+				<li ><a tabindex="-1" href="/other-services/broadband/"><i class="fa fa-server fa-fw"></i> Broadband Internet</a></li>
+				</ul>
+        </li>
+
+          <li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes"><i class="fa fa-exchange"></i> API <span class="caret"></span></a>
+			<ul class="dropdown-menu" aria-labelledby="themes">
+				<li ><a tabindex="-1" href="/api/"><span class="fa fa-sitemap fa-fw"></span> Overview</a></li>
+				<li ><a tabindex="-1" href="/api/docs/first-connect/hello-world/"><i class="fa fa-hdd-o fa-fw"></i> First Connect</a></li>
+				<li ><a tabindex="-1" href="/api/docs/account/name-detail/"><i class="fa fa-dollar fa-fw"></i> Account</a></li>
+				<li ><a tabindex="-1" href="/api/docs/1300-numbers/available-1300-numbers/"><i class="fa fa-gift fa-fw"></i> 1300/1800 Numbers</a></li>
+					</ul>
+        </li>
+
+          <li >
+			<a  href="http://wiki.telecube.com.au" target="_blank" id="themes"><i class="fa fa-life-ring"></i> Wiki <i class="fa fa-external-link"></i></span></a>
+        </li>
+
+    </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+                <li><a href="https://manage.telecube.com.au/myaccount/detail/"><span class="fa fa-user"></span> My Account</a></li>
+      </ul>
+
+    </div>
+  </div>
+</div>
+
+
+
+    <div class="container">
+
+      <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-9">
+            <h1><i class="fa fa-phone"></i> Broadband - Critical Information</h1>
+            <p class="lead">This is a critical information summary for broadband services.</p>
+          </div>
+
+          <div class="col-lg-3">
+                          <button type="button" class="btn btn-lg btn-success pull-right"><i class="fa fa-phone"></i>  Call Now: 13 28 23</button>
+          </div>
+
+        </div>
+      </div>
+
+
+        <div class="row">
+		             
+          <div class="col-lg-3 pull-right">
+            <h4><i class="fa fa-list-ul fa-fw"></i> 1300 Numbers Quick Nav</h4>
+              <ul class="nav nav-pills nav-stacked" style="max-width: 300px;">
+                <li ><a href="/1300-numbers/"><span class="fa fa-sitemap fa-fw"></span> Overview</a></li>
+                <li ><a href="/1300-numbers/features/"><span class="fa fa-folder-open fa-fw"></span> Features</a></li>
+                <li ><a href="/1300-numbers/prices/"><span class="fa fa-dollar fa-fw"></span> Prices</a></li>
+                <li ><a href="/1300-numbers/available-1300-numbers/"><span class="fa fa-bolt fa-fw"></span> Available 1300 Numbers</a></li>
+                <li ><a href="/1300-numbers/faq/"><span class="fa fa-question-circle fa-fw"></span> Frequently Asked Questions</a></li>
+                <li ><a tabindex="-1" href="/1300-numbers/smart-numbers/"><span class="fa fa-sort-numeric-asc fa-fw"></span> Smart Numbers</a></li>
+              </ul>
+          
+          <p>&nbsp;</p>
+          </div>
+
+
+
+
+			<div class="col-lg-9">
+	            <div class="panel panel-primary">
+	              <div class="panel-heading">
+	                <h3 class="panel-title">Information about the service</h3>
+	              </div>
+	              <div class="panel-body">
+
+					<div class="table-responsive">
+		              <table class="table table-striped table-bordered">
+		                <tbody>
+		                  <tr>
+		                    <td width="20%">Service Description</td>
+		                    <td>Broadband internet is delivered on either ADSL or NBN infrastructure provided by the carriers we resell through.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Minimum term</td>
+		                    <td>No contract</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Offer Inclusions</td>
+		                    <td>
+		                    	<ul>
+		                    		<li>Data allowance according to selected plan.</li>
+		                    		<li>Speed shaping to 256kbps when over data limit.</li>
+		                    		<li>Static IP address included on all plans.</li>
+		                    		<li>Data bundles can be purchased on request.</li>
+		                    		<li>Online data usage reporting</li>
+		                    	</ul>
+		                    </td>
+		                  </tr>
+		                  <tr>
+		                    <td>Offer Exclusions</td>
+		                    <td>
+		                    	<ul>
+		                    		<li>No line rental included with ADSL plans.</li>
+		                    		<li>No modem is included with any broadband plans.</li>
+		                    	</ul>
+		                    </td>
+		                  </tr>
+		                  <tr>
+		                    <td>Important conditions</td>
+		                    <td>This is a prepaid service and you are required to ensure there is credit in your account to cover the monthly cost of the broadband service in advance. However, this does not mean that if you do not top up your account that it will automatically be cancelled. You will continue to be billed for the Service until you contact us to cancel the service.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Important limitations</td>
+		                    <td><p>Service availablility is reliant on our carriers having infrastructure available to service your location.</p></td>
+		                  </tr>
+		                  <tr>
+		                    <td>Important restrictions</td>
+		                    <td>&nbsp;</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Important qualifications</td>
+		                    <td>To use the ADSL broadband service you will need a PSTN on which the service can be delivered.</td>
+		                    <td>Both upload and download data is included in data usage.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Important Recommendations</td>
+		                    <td>&nbsp;</td>
+		                  </tr>
+		                </tbody>
+		              </table>
+		            </div>
+
+	              </div>
+	            </div>
+	            
+	            <div class="panel panel-primary">
+	              <div class="panel-heading">
+	                <h3 class="panel-title">Information about the pricing</h3>
+	              </div>
+	              <div class="panel-body">
+
+					<div class="table-responsive">
+		              <table class="table table-striped table-bordered">
+		                <tbody>
+		                  <tr>
+		                    <td width="25%">Setup Fees</td>
+		                    <td>$0</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Minimum monthly charge</td>
+		                    <td>One months cost depending on the plan you select.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Maximum monthly charge</td>
+		                    <td>NA</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Early termination charge</td>
+		                    <td>Services cancelled within 6 months attract a $99 cancellation fee.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>Speed change charge</td>
+		                    <td>Changing to a lower plan incurs a $38.50 change fee, to a higher plan is free.</td>
+		                  </tr>
+		                </tbody>
+		              </table>
+		            </div>
+
+	              </div>
+	            </div>
+	            
+	            
+	            <div class="panel panel-primary">
+	              <div class="panel-heading">
+	                <h3 class="panel-title">Plan pricing summary</h3>
+	              </div>
+	              <div class="panel-body">
+
+					<div class="table-responsive">
+		              <table class="table table-striped table-bordered">
+	            <legend>ADSL only</legend>
+                <thead>
+                  <tr>
+                    <th>Data Allowance</th>
+                    <th>AAPT Network^</th>
+                    <th>Telstra Network^</th>
+                    <th>Optus Naked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>10 GB</td>
+                    <td>$29.70</td>
+                    <td>$38.50</td>
+                    <td>$42.90</td>
+                  </tr>
+                  <tr>
+                    <td>100 GB</td>
+                    <td>$40.70</td>
+                    <td>$49.50</td>
+                    <td>$53.90</td>
+                  </tr>
+                  <tr>
+                    <td>250 GB</td>
+                    <td>$51.70</td>
+                    <td>$60.50</td>
+                    <td>$64.90</td>
+                  </tr>
+                  <tr>
+                    <td>500 GB</td>
+                    <td>$62.70</td>
+                    <td>$71.50</td>
+                    <td>$75.90</td>
+                  </tr>
+                  <tr>
+                    <td>1000 GB</td>
+                    <td>$73.70</td>
+                    <td>$82.50</td>
+                    <td>$86.90</td>
+                  </tr>
+                </tbody>
+
+		              </table>
+		            </div>
+
+					<div class="table-responsive">
+		              <table class="table table-striped table-bordered">
+			            <legend>NBN Fixed / Wireless^</legend>
+		                <thead>
+		                  <tr>
+		                    <th>Data Allowance</th>
+		                    <th>12/1 Mbps^</th>
+		                    <th>25/5 Mbps^</th>
+		                    <th>100/40 Mbps</th>
+		                  </tr>
+		                </thead>
+		                <tbody>
+		                  <tr>
+		                    <td>10 GB</td>
+		                    <td>$39.60</td>
+		                    <td>$50.60</td>
+		                    <td>$72.60</td>
+		                  </tr>
+		                  <tr>
+		                    <td>200 GB</td>
+		                    <td>$49.50</td>
+		                    <td>$60.50</td>
+		                    <td>$82.50</td>
+		                  </tr>
+		                  <tr>
+		                    <td>500 GB</td>
+		                    <td>$57.75</td>
+		                    <td>$68.75</td>
+		                    <td>$90.75</td>
+		                  </tr>
+		                  <tr>
+		                    <td>3000 GB</td>
+		                    <td>$65.45</td>
+		                    <td>$76.45</td>
+		                    <td>$98.45</td>
+		                  </tr>
+		                </tbody>
+		              </table>
+		            </div>
+
+	              </div>
+	            </div>
+	            
+	            <div class="panel panel-primary">
+	              <div class="panel-heading">
+	                <h3 class="panel-title">Other Information</h3>
+	              </div>
+	              <div class="panel-body">
+
+					<div class="table-responsive">
+		              <table class="table table-striped table-bordered">
+		                <tbody>
+		                  <tr>
+		                    <td width="25%">Access your data usage</td>
+		                    <td>You can access your data usage by going to <a href="https://manage.telecube.com.au/myaccount/detail/" target="_blank">My Account</a></td>
+		                  </tr>
+		                  <tr>
+		                    <td>Customer service contact</td>
+		                    <td>During business hours 9am to 5pm weekdays call <b>13 28 23</b> or <b>+61 3 9097 7770</b> or email <a href="mailto:support@telecube.com.au">support@telecube.com.au</a> any time.</td>
+		                  </tr>
+		                  <tr>
+		                    <td>TIO contact details</td>
+		                    <td><p>We will work hard to rectify any problem you might have with us or our service, if however you have exhausted all avenues for resolving your complaint and aren't satisfied with the suggested remedies, you can contact the Telecommunications Industry Ombudsman (TIO) by phone on 1800 062 058</p>
+		                    	<p>For full contact details, visit: <a href="http://www.tio.com.au/about-us/contact-us" target="_blank">http://www.tio.com.au/about-us/contact-us</a></p>
+		                    </td>
+		                  </tr>
+		                </tbody>
+		              </table>
+		            </div>
+
+	              </div>
+	            </div>
+          </div>
+
+
+
+
+
+        </div>
+
+
+
+			
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            
+            <ul class="list-unstyled">
+              <li>
+				<address>
+					<strong>Telecube Pty Ltd</strong><br>
+					530 Little Collins St<br>
+					Melbourne, Vic, 3000<br>
+					<abbr title="Phone"><i class="fa fa-phone fa-fw"></i> Phone:</abbr> <a href="tel:132823">13 28 23</a><br>
+					<abbr title="Fax"><i class="fa fa-print fa-fw"></i> Fax:</abbr> 1300 211 800<br>
+					<abbr title="Email"><i class="fa fa-envelope fa-fw"></i> Email:</abbr> <a href="mailto:info@telecube.com.au?subject=Website Contact">info@telecube.com.au</a><br>
+					<abbr title="Skype"><i class="fa fa-skype fa-fw"></i> Skype:</abbr> <a href="skype:telecube.com.au?call">telecube.com.au</a>
+				</address>
+			</li>
+              <li><a href="/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="/terms-and-conditions/">Terms & Conditions</a></li>
+              <li><a href="/complaints-handling/">Complaints</a></li>
+              <li><a href="/billing-payments/">Billing</a></li>
+              <li><a href="/network-information/">Network</a></li>
+              <li><a href="/affiliate/">Affiliate</a></li>
+              <li><a href="/contact-us/">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+        
+      </footer>
+
+
+    </div>
+
+	    <script src="/assets/bower_components/jquery/jquery.min.js"></script>
+    <script src="/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bootswatch.js"></script>
+    <script src="/assets/js/common.js"></script>
+	
+	
+
+<!-- Google Code for Remarketing Tag -->
+<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 989723991;
+	var google_custom_params = window.google_tag_params;
+	var google_remarketing_only = true;
+	/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+<noscript>
+	<div style="display:inline;">
+		<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/989723991/?value=0&amp;guid=ON&amp;script=0"/>
+	</div>
+</noscript>
+
+	  </body>
+</html>
+    
