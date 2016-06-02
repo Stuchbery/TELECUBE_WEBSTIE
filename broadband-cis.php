d388229dd626b55d5d693b1a014356e25b58fe6d
Stuchbery <jamesmidship@gmail.com>
2016-06-02 15:24:59 +1000

    Add files via upload

--- broadband-cis.php
+++ broadband-cis.php
@@ -6,16 +6,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<meta charset="utf-8">
 
-	    <link rel="stylesheet" href="/assets/css/bootstrap.css" media="screen">
-		<link rel="stylesheet" href="/assets/css/bootswatch.min.css">
-		<link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
+	    <link rel="stylesheet" href="./assets/css/bootstrap.css" media="screen">
+		<link rel="stylesheet" href="./assets/css/bootswatch.min.css">
+		<link rel="stylesheet" href="./assets/font-awesome/css/font-awesome.min.css">
 
-		<link rel="stylesheet" href="/assets/bower_components/bootstrap/less/responsive-utilities.less">
+		<link rel="stylesheet" href="./assets/bower_components/bootstrap/less/responsive-utilities.less">
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
-		  <script src="/assets/bower_components/bootstrap/assets/js/html5shiv.js"></script>
-		  <script src="/assets/bower_components/bootstrap/assets/js/respond.min.js"></script>
+		  <script src="./assets/bower_components/bootstrap./assets/js/html5shiv.js"></script>
+		  <script src="./assets/bower_components/bootstrap./assets/js/respond.min.js"></script>
 		<![endif]-->
 
 		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
@@ -424,10 +424,10 @@
 
     </div>
 
-	    <script src="/assets/bower_components/jquery/jquery.min.js"></script>
-    <script src="/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="/assets/js/bootswatch.js"></script>
-    <script src="/assets/js/common.js"></script>
+	    <script src="./assets/bower_components/jquery/jquery.min.js"></script>
+    <script src="./assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./assets/js/bootswatch.js"></script>
+    <script src="./assets/js/common.js"></script>
 	
 	
 
